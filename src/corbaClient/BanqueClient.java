f19d796a41b31da5967f0cc8d7b8c184d4a7e1d1
Hachem souissi <Hachemsouissi1@gmail.com>
2025-11-11 23:19:29 +0100

    2

--- src/corbaClient/BanqueClient.java
+++ src/corbaClient/BanqueClient.java
@@ -1,4 +1,29 @@
 package corbaClient;
 
+import corbaBanque.Compte;
+import corbaBanque.IBanqueRemote;
+import corbaBanque.IBanqueRemoteHelper;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
 public class BanqueClient {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Démarrage du Client CORBA Banque");
+            Context ctx = new InitialContext();
+            Object ref = ctx.lookup("BK");
+            IBanqueRemote stub= IBanqueRemoteHelper.narrow((org.omg.CORBA.Object)ref);
+            Compte c1 = new Compte(1, 1000);
+            Compte c2 = new Compte(2, 200);
+            stub.creerCompte(c1);
+            stub.creerCompte(c2);
+            System.out.println("Comptes créés avec succès.");
+            stub.verser(100, 1);
+            System.out.println("Versement de 100  sur le compte 1.");
+            System.out.println("Résultat de conversion :"+stub.conversion(600));
+
+        }catch (Exception e) { e.printStackTrace(); }
+    }
 }
+
